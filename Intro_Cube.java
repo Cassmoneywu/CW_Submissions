ebb21d4044c62c8894e1a379303383bf02ee4657
Cassmoneywu <cassidyhurban@gmail.com>
2024-08-30 13:09:37 -1000

    Create Intro_Cube.java

---
+++ Intro_Cube.java
@@ -0,0 +1,26 @@
+public class Intro_Cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*
+	 * This code is used to declare a side length first as a int but then after was used to declare it as a double same with the volume.  
+	 */
+	
+	//Task 1 is suppose calculate the surface area 
+	double side=9.0;
+	double surface=6*side*side;
+	double surfacef=surface;
+	
+	
+	//Task 2 is used to calculate the volume of a cube 
+	double volume=side*side*side;
+	double volumef=volume;
+	
+	
+	//Task is to print out the values as doubles in a sentence 
+    System.out.print("the surface of the cube is " + (int) surfacef );
+    System.out.print(" the volume of the cube is " + (int) volumef);	
+	
+	}
+
+}
