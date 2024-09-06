16026ed1076cf4ccdcde2ef1f74854cac3813e17
Cassmoneywu <cassidyhurban@gmail.com>
2024-09-06 13:06:50 -1000

    Create Practicing_System_And_Primitives.java

---
+++ Practicing_System_And_Primitives.java
@@ -0,0 +1,31 @@
+public class Practacing_System_And_primitives {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Task 1: This code is used to print out my name "CASSIDY" using the number "0"
+		System.out.println("  0 0 0    0 0     0 0 0   0 0 0   0 0 0   0 0 0    0     0");
+		System.out.println(" 0       0     0   0       0         0     0    0   0     0");
+		System.out.println(" 0       0 0 0 0   0 0 0   0 0 0     0     0    0    0 0 0 ");
+		System.out.println(" 0       0     0       0       0     0     0    0      0   ");
+		System.out.println("  0 0 0  0     0   0 0 0   0 0 0   0 0 0   0 0 0       0   ");
+				
+		//Task 2: this code is used to do the math problem 
+		double prod1=6.0*3.5;
+    	double prod2=1.5*5;
+    	double top=prod1-prod2;
+    	double bot=55.6-30.2;
+    	double answ=top/bot;
+    	System.out.println(answ);
+			
+				
+	//Task 3: this code is used to print out the circumference and area of a circle
+				
+    	double radius=4.0;
+		double two=2;
+		double pie=3.14;
+		double circum=two*pie*radius;
+		System.out.println(circum);
+		
+		double rs=radius*radius;
+		double area=pie*rs;
+		System.out.print(area);
