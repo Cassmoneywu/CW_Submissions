e969cf5d319d24162e43144ea78c691e536196ea
Cassmoneywu <cassidyhurban@gmail.com>
2024-10-30 10:46:09 -1000

    Create In_1_To_10.java

---
+++ Coding_Bat_Logic_2/In_1_To_10.java
@@ -0,0 +1,17 @@
+public boolean in1To10(int n, boolean outsideMode) {
+ if(outsideMode == false)
+ {
+   if(n<=10 && n>=1)
+   {
+     return true;
+   }
+ }
+  else if(outsideMode)
+  {
+    if(n<=1 || n>=10)
+    {
+      return true;
+    }
+  }
+  return false;
+}
