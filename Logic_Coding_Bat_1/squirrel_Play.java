2181b288079dbd5509aa5afa8fb4f1607d86951e
Cassmoneywu <cassidyhurban@gmail.com>
2024-11-06 08:52:09 -1000

    Update squirrel_Play.java

--- Logic_Coding_Bat_1/squirrel_Play.java
+++ Logic_Coding_Bat_1/squirrel_Play.java
@@ -1,1 +1,31 @@
-
+public boolean squirrelPlay(int temp, boolean isSummer) 
+{
+  if(isSummer == true)
+  {
+    if(temp >= 60 && temp <= 100)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  if(isSummer== false)
+  {
+    if(temp >=60 && temp <= 90)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else
+  {
+    return false;
+  }
+   
+}
