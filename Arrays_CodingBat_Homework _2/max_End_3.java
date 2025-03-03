377d4e0f4bf057be86249dd896af4da0a7d54d5c
Cassmoneywu <cassidyhurban@gmail.com>
2025-03-02 21:49:30 -1000

    Update max_End_3.java

--- Arrays_CodingBat_Homework _2/max_End_3.java
+++ Arrays_CodingBat_Homework _2/max_End_3.java
@@ -1,1 +1,12 @@
+public int[] maxEnd3(int[] nums) {
+  int max = nums[0];
+    
+    if(nums[2]> max)
+    max = nums[2];
+  
+  for(int i= 0; i<nums.length; ++i)
+    nums[i] = max;
+  
+  return nums;
+}
 
