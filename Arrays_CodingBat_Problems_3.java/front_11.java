87ca7cea38bd3f5d95c1c80587bebaa00a7eb341
Cassmoneywu <cassidyhurban@gmail.com>
2025-03-13 22:35:01 -1000

    Create front_11.java

---
+++ Arrays_CodingBat_Problems_3.java/front_11.java
@@ -0,0 +1,20 @@
+public int[] front11(int[] a, int[] b) {
+  int[] arr1 = new int[] {b[0]};
+  {
+    return arr1;
+  }
+  if(b.length == 0)
+  {
+    int[] arr2 = new int[] {a[0]};
+    return arr2;
+  }
+  
+  if(a.length === 0 && b.length == 0)
+  {
+    return a;
+  }
+  if(a.length == 0)
+  }
+  int [] arr = new int [] {a[0], b[0]};
+    return arr;
+}
