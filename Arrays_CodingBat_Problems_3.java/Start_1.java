eb7be38fb158cdcced06664985d1001d3ceaa6be
Cassmoneywu <cassidyhurban@gmail.com>
2025-03-13 22:38:50 -1000

    Create Start_1.java

---
+++ Arrays_CodingBat_Problems_3.java/Start_1.java
@@ -0,0 +1,14 @@
+public int start1(int[] a, int[] b) {
+  int cLet = 0;
+  If(a.length !=0)
+  {
+    if(a[0] == 1)
+    cLet++;
+  }
+  if(b.length != 0)
+  {
+    if(b[0] == 1)
+    cLet++;
+  }
+  return cLet
+}
