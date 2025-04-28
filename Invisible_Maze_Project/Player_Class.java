090f69aa3cad36ef5ec3ec30d2411c4569e2fd75
Cassmoneywu <cassidyhurban@gmail.com>
2025-04-28 13:34:23 -1000

    Create Player_Class.java

---
+++ Invisible_Maze_Project/Player_Class.java
@@ -0,0 +1,33 @@
+
+public class Player {
+
+int xpos;
+int ypos;
+
+	
+public Player()
+{
+	xpos= 0;
+	ypos=0;
+}
+
+public int getX()
+{
+	return xpos;
+}
+public int getY()
+{
+	return ypos;
+}
+
+public void setX(int newX)
+	{
+	xpos = newX;
+	}
+public void setY(int newY)
+{
+	ypos = newY;
+	}
+
+}
+
